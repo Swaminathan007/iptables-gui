632c1e3e39d57854b8772f3ef48d697651b41e38
Swaminathan007 <swaminathang12b2svv@gmail.com>
2024-10-26 23:55:42 +0530

    Setup file and gitignore update

--- setup.sh
+++ setup.sh
@@ -6,5 +6,8 @@ sudo systemctl start iptables
 sudo mkdir certs
 sudo chmod 777 -R certs/
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
+sudo cp key.pem certs/
+sudo cp cert.pem certs/
+sudo rm -rf *.pem
 sudo pip3 install flask flask-wtf pam
 sudo python3 app.py
